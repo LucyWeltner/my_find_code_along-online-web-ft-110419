ac68ba63c43c931cca32b54ccf4be1af284e5bcb
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 18:44:17 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length do 
-    if yield(collection[i]) == true 
+    if yield(collection[i]) 
       return collection[i]
     else 
       i += 1 
